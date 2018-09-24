2f62e20b23ffa45fa71e00b4fbf22ada0b66cb4b
chung yang <chung.yang@engineer.uconn.edu>
2018-09-24 19:56:54 -0400

    plt.show() is a blocking call, use draw instead in each loop, and show the figures all at once

--- HW1_submit/q3_27_imagesvd.py
+++ HW1_submit/q3_27_imagesvd.py
@@ -13,7 +13,7 @@ Wenting Wang 31930946
 
 """
 
-import cv2
+from scipy import misc
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.linalg import norm
@@ -25,11 +25,12 @@ def showImg(image):
     print('Input image:')
     plt.imshow(image, cmap='gray')
     plt.title("The input image")
-    plt.show()
+    plt.draw()
     print(' ')
 
 # Perform SVD, then reconstruct image in several levels and show it
-def reconImg(image, pct):
+def reconImg(image, pct, figure_n):
+    plt.figure(figure_n)
     # SVD
     n, m = image.shape
     u, s, vh = svd(image, full_matrices=True)
@@ -37,6 +38,7 @@ def reconImg(image, pct):
     
     # Reconstruct
     idx = int(min(n,m) * pct)
+    print(idx)
     img_recon = u[:, :idx].dot(np.diag(s[:idx])).dot(vh[:idx,:])
     print('Use Top {:.0%} of the singular values.'.format(pct), '\n')
 
@@ -48,15 +50,18 @@ def reconImg(image, pct):
     plt.imshow(img_recon, cmap='gray')
     plt.title('Top {:.0%} of the singular values.'.format(pct) + '\n' + \
               '{:.5%} percent of the Forbenius norm is captured.'.format(fb_norm_pct))
-    plt.show()
+    plt.draw()
 
 
 if __name__ == "__main__":
     # Read Image
     # (put the image file to the folder os.getcwd())
-    img = cv2.imread('image.jpg', 0)
+    img = misc.imread('image.jpg', 1)
     showImg(img) # show the input image
     
     percentages = [0.05, 0.10, 0.25, 0.50]
+    figure_n = 1
     for pct in percentages:
-        reconImg(img, pct) # show the reconstructed image
+        reconImg(img, pct, figure_n) # show the reconstructed image
+        figure_n += 1
+    plt.show()
